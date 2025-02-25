e32b1ca6adf175d745b69485b861721d02ae5188
Lasanga HGC <10898545@students.plymouth.ac.uk>
2025-02-25 13:17:09 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/luo3_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.luo3_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorsOfApp {
+  static const Color primary = Color(0xFF104879); // Your main app color
+  static const Color secondary = Color(0xFF1E88E5);
+  static const Color accent = Color(0xFFFFC107);
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color textPrimary = Color(0xFF212121);
+  static const Color textSecondary = Color(0xFF757575);
+}
