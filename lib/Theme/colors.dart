4dc1fa5f563cba102dbf39946619ddbc5cd6c871
Lasanga HGC <10898545@students.plymouth.ac.uk>
2025-02-25 13:56:09 +0530

    Color Class Update

--- lib/Theme/colors.dart
+++ lib/Theme/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ColorsOfApp {
+class Luo3Colors {
   static const Color primary = Color(0xFF104879); // Your main app color
   static const Color secondary = Color(0xFF1E88E5);
   static const Color accent = Color(0xFFFFC107);
