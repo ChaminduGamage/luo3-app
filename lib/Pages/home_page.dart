6df1f9e7fb16774910a4563f03711af5cd49ee7a
Lasanga HGC <chamindulgamage@gmail.com>
2025-03-24 23:21:46 +0530

    Implement home page

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,13 +1,218 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:luo3_app/Theme/colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isDrinkingModeOn = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 210,
+              decoration: const BoxDecoration(
+                color: Luo3Colors.primary,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20,
+                      left: 20,
+                      right: 20,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Drinking Mode',
+                          style: GoogleFonts.inter(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                            color: Luo3Colors.inputBackground,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isDrinkingModeOn = !isDrinkingModeOn;
+                            });
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(milliseconds: 200),
+                            width: 40, // Reverted to previous width
+                            height: 20, // Reverted to previous height
+                            decoration: BoxDecoration(
+                              color: isDrinkingModeOn
+                                  ? Luo3Colors.accent
+                                  : Luo3Colors.textSecondary, // Track color
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(
+                                  color: Luo3Colors.inputBackground,
+                                  width: 2), // Border color & width
+                            ),
+                            child: Stack(
+                              alignment: Alignment.centerLeft,
+                              children: [
+                                AnimatedPositioned(
+                                  duration: const Duration(milliseconds: 200),
+                                  left: isDrinkingModeOn
+                                      ? 21
+                                      : 3, // Adjusted positioning slightly to avoid touching border
+                                  child: Container(
+                                    width: 14, // Reverted thumb size
+                                    height: 14,
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Luo3Colors
+                                          .inputBackground, // Thumb color
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Location',
+                          style: GoogleFonts.inter(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: Luo3Colors.inputBackground,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              width: 30,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                color: Luo3Colors.inputBackground,
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: const Icon(
+                                Icons.location_on,
+                                color: Luo3Colors.primary,
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Text(
+                              'Your Location',
+                              style: GoogleFonts.inter(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                                color: Luo3Colors.inputBackground,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 30,
+                              height: 30,
+                              child: Icon(
+                                Icons.arrow_drop_down,
+                                color: Luo3Colors.inputBackground,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            color: Luo3Colors.inputBackground,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: const Icon(
+                            Icons.notification_important,
+                            color: Luo3Colors.primary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 300,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            color: Luo3Colors.inputBackground,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              const SizedBox(width: 10),
+                              const Icon(
+                                Icons.search,
+                                color: Luo3Colors.textSecondary,
+                              ),
+                              const SizedBox(width: 10),
+                              Text(
+                                'Search for a drink',
+                                style: GoogleFonts.inter(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                  color: Luo3Colors.textSecondary,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: 45,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            color: Luo3Colors.accent,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Icon(
+                            Icons.filter_list,
+                            color: Luo3Colors.textPrimary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
